83c07be5cceacabbae24597b96a927a1828b265e
justutpal <kumarutpal2112001@gmail.com>
2024-07-26 16:38:08 +0530

    All done

---
+++ javascript.js
@@ -0,0 +1,55 @@
+let demo = document.querySelector(".demo");
+let strt = document.querySelector(".strt");
+let whole = document.querySelector(".whole");
+let startName = document.querySelector(".startName");
+let user = document.querySelector(".user");
+let button = document.querySelector(".button");
+let cCopy = document.querySelector(".cCopy");
+let eCopy = document.querySelector(".eCopy");
+let iCopy = document.querySelector(".iCopy");
+let copyWhole = document.querySelector(".copyWhole");
+
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+let startnum = "9876"
+let str = "1234567890";
+let number = " "
+let another = " "
+let newNameEmail = " "
+
+button.addEventListener('click' , () => {
+    another = ""
+    number = ""
+    newNameEmail = ""
+
+    for (let i = 0; i < 2; i++) {
+        another += startnum[Math.floor(Math.random() * 4)]        
+    }
+    for (let i = 0; i < 8; i++) {
+        number += str[Math.floor(Math.random() * 10)]        
+    }
+    whole.innerHTML = number;
+    strt.innerHTML = another;
+
+    for (let i = 0; i < 8; i++) {
+        newNameEmail += alphabet[Math.floor(Math.random() * 26)]      
+    }
+    startName.innerHTML = newNameEmail;
+    user.innerHTML = newNameEmail;
+})
+
+cCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(another + number)
+});
+
+iCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(newNameEmail)
+})
+
+eCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(newNameEmail + "@gmail.com")
+})
+
+copyWhole.addEventListener('click', () => {
+    navigator.clipboard.writeText(another+number + " " + newNameEmail + " " + newNameEmail + "@gmail.com" )
+})
